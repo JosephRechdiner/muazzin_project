9d539d53ab6680925f342ba43ae1e75d59a97848
JosephRechdiner <jrechdiner2002@gmail.com>
2026-03-01 19:14:13 +0200

    added main.py file and made sure everything is working with docker compose

--- processor-service/app/processor_config.py
+++ processor-service/app/processor_config.py
@@ -2,16 +2,23 @@ from logging import Logger
 import os
 
 class ProcessorConfig:
+    """
+    Config class is supposed to load env variables that are needed for this service
+    """
     def __init__(self, logger: Logger):
         self.logger = logger
         self.bootstrap_servers = os.getenv("BOOTSTRAP_SERVERS")
         self.listen_topic = os.getenv("LISTEN_TOPIC")
         self.group_id = os.getenv("GROUP_ID")
         self.database_name = os.getenv("DATABASE_NAME")
-        self.collection_name = os.getenv("COLLECTION_NAME")
         self.elastic_uri = os.getenv("ELASTIC_URI") 
+        self.index_name = os.getenv("INDEX_NAME") 
+        self.mongo_uri = os.getenv("MONGO_URI") 
         
     def validate(self):
+        """
+        Validating env variables exist
+        """
         missing = []
 
         if not self.bootstrap_servers:
@@ -22,10 +29,12 @@ class ProcessorConfig:
             missing.append("GROUP_ID")
         if not self.database_name:
             missing.append("DATABASE_NAME")
-        if not self.collection_name:
-            missing.append("COLLECTION_NAME")
         if not self.elastic_uri:
             missing.append("ELASTIC_URI")
+        if not self.index_name:
+            missing.append("INDEX_NAME")
+        if not self.mongo_uri:
+            missing.append("MONGO_URI")
 
         if missing:
             msg = "|".join(missing) + " missing"
--- test.py
+++ test.py
@@ -5,6 +5,48 @@ import os
 import gridfs
 from pymongo import MongoClient
 import json
+from shared.models import FileMetadata
+from elasticsearch import Elasticsearch
+import logging
+from logging import Logger
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('test-service')
+
+class ElasticClient:
+    def __init__(self, index_name: str, elastic_uri: str, logger: Logger):
+        self.index_name = index_name
+        self.logger = logger
+        try:
+            self.es = Elasticsearch(elastic_uri)
+            self.logger.info(f"Connected to Elastic, {self.es}")
+        except Exception as e:
+            self.logger.exception(f"Could not connect to ElasticSearch, Error: {str(e)}")
+        
+    def create_index(self):
+        mapping = {
+            "mappings": {
+                "properties": [
+                    {"file_path": {"type": "keyword"}},
+                    {"file_name": {"type": "keyword"}},
+                    {"file_size": {"type": "integer"}},
+                    {"file_format": {"type": "keyword"}},
+                    {"created_at": {"type": "keyword"}}
+                ]
+            }
+        }
+        try:
+            if not self.es.indices.exists(index=self.index_name):
+                self.es.indices.create(index=self.index_name, body=mapping)
+        except Exception as e:
+            self.logger.exception(f"Could not create {self.index_name}, Error: {str(e)}")
+
+    def add_to_index(self, metadata: dict):
+        try:
+            self.es.update(index=self.index_name, body=metadata)
+        except Exception as e:
+            self.logger.error(f"Could not insert {metadata}, Error: {str(e)}")
+
 
 podcats_paths = glob.glob("podcasts/*.wav")
 
@@ -12,6 +54,9 @@ client = MongoClient("mongodb://localhost:27017")
 database = client["podcasts_metadatas"]
 
 fs = gridfs.GridFS(database)
+
+client = ElasticClient('test', 'http://elasticsearch:9200', logger)
+client.create_index()
 
 image_id = "1"
 
@@ -34,10 +79,12 @@ for podcast_path in podcats_paths:
     created_at_full_date = datetime.fromtimestamp(created_at_seconds).strftime("%A, %B %d, %Y %I:%M:%S")
     metadata["created_at"] = created_at_full_date
     
+    file_metadata = FileMetadata(**metadata)
+
     with open(file_path, "rb") as file:
         data = file.read()
     fs.put(data=data, id=image_id)
+
+    client.add_to_index(file_metadata.model_dump())
+
     image_id = str(int(image_id) + 1)
-
-
-
